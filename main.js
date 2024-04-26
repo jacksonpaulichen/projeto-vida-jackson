a572428e6929908688f961a04af80608d4e0e685
jackson paulichen <108669034+jacksonpaulichen@users.noreply.github.com>
2024-04-26 10:47:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const botoes = document.querySelectorAll("botao");
-const textox = docuent. querySelectorAll(".aba conteudo")
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i=0; i<botoes.length; i++) {
 botoes[i].onclick = function() {
@@ -10,29 +10,47 @@ textos[j].classList.remove("ativo");
 }
 
 botoes[i].classList.add("ativo");
-textos[j].classList.add("ativo");
+textos[i].classList.add("ativo");
 }
 }
 
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-21T00:00:00");
+const tempoObjetivo2 = new Date("2024-08-09T00:00:00");
+const tempoObjetivo3 = new Date("2024-06-10T00:00:00");
+const tempoObjetivo4 = new Date("2024-09-15T00:00:00");
 
-const tempoObjetivo1 = new Date("2024-07-05T00:00:00")
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-contadores [0].textContent = calularTempo (tempoObjetivo1)
+contadores[0].textContent = calculaTempo (tempoObjetivo1);
 
-function calcula (tempoObjetivo1){
-    const tempoAtual = new Date ();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = math.floor (tempoFinal/1000);
-    let minutos = math.floor(segundos/60);
-    let horas = math.floor(minutos/60);
-    let dias = math.floor (horas/24);
-    
-    segundos %= 60
-    minutos%= 60
-    horas%= 24
+function calculaTempo (tempoObjetivo){
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
 
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
 
-
-
+if (tempoFinal>0){
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else {
+    return "Bom trabalho, você conseguiu!"
 }
+}
+
+function atualizaCronometro(){
+    for(let i=0; i<contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
